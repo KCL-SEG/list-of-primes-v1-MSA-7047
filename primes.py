4519b38b09c99e49c3e69b0ed0e091b1f714587e
Sameen Ahmed <sameenahmed7047@gmail.com>
2023-10-11 16:54:42 +0100

    Complete task to list x number of primes.

--- primes.py
+++ primes.py
@@ -3,4 +3,23 @@
 
 def primes(number_of_primes):
     list = []
+    num = 2
+    while len(list) < number_of_primes:         # makes sure the list is equal to the given parameter 
+        if is_prime(num):
+            list.append(num)
+        num += 1
+    print(list)
     return list
+
+def is_prime(n):            # checks if a number is prime, returns True if it is and False if not
+    if n == 2 or n == 3:
+        return True
+    elif n <= 1 or n % 2 == 0 or n % 3 == 0:
+        return False
+
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
